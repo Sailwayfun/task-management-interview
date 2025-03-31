cf01ae50ee111079a3b013b1dcd0cb58dd3eccbb
Sail_Liao <124685991+Sailwayfun@users.noreply.github.com>
2025-03-31 11:45:15 +0000

    init

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["qjmwfx-5173.csb.app"],
+    allowedHosts: ["z3n4xr-5173.csb.app"],
   },
 });
